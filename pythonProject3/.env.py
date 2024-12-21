60e58a76b42e6bf0d71d8c525b16bbc6a0093beb
adarshrajs1999 <153977458+adarshrajs1999@users.noreply.github.com>
2024-12-21 20:06:37 +0530

    today

---
+++ pythonProject3/.env.py
@@ -0,0 +1,13 @@
+# Django settings
+SECRET_KEY='your_actual_secret_key'
+DEBUG=True
+ALLOWED_HOSTS='yourdomain.com,www.yourdomain.com'
+
+# Database settings
+DB_NAME='fresh_django_project'
+DB_USER='postgres'
+DB_PASSWORD='1234'
+DB_HOST='127.0.0.1'  # or the actual host IP (e.g., db_host_url)
+DB_PORT='5432'
+
+# Add any other environment variables as needed
